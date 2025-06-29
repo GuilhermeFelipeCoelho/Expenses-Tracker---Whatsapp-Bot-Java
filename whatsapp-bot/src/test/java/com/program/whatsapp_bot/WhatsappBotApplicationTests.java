0adf2489f4128e810d2cb0a280ccf04f2989d216
Guilherme Felipe <glipecoelho2@gmail.com>
2025-06-29 19:58:50 -0300

    start the project
    initial project setup using Spring Initializr with base dependencies

---
+++ whatsapp-bot/src/test/java/com/program/whatsapp_bot/WhatsappBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.program.whatsapp_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
