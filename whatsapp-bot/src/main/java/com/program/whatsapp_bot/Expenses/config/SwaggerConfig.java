9bad7e0491ffded3d37b73a92735b5a939f7ff30
Guilherme Felipe <glipecoelho2@gmail.com>
2025-07-28 05:32:54 -0300

    Update SwaggerConfig.java

--- whatsapp-bot/src/main/java/com/program/whatsapp_bot/Expenses/config/SwaggerConfig.java
+++ whatsapp-bot/src/main/java/com/program/whatsapp_bot/Expenses/config/SwaggerConfig.java
@@ -10,7 +10,7 @@ import io.swagger.v3.oas.models.info.Info;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI apiOpenAPI() {
+    public OpenAPI whatsappExpenseTrackerOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("API WhatsApp Expense Tracker")
