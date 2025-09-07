95a0e18422cd19f6476e2954837f4ec5c4d70c2b
PotenzaFelip <potenzafelipe@gmail.com>
2025-09-06 21:42:45 -0300

    correções de conflitos

--- whatsapp-bot/src/main/java/com/program/whatsapp_bot/Expenses/model/User.java
+++ whatsapp-bot/src/main/java/com/program/whatsapp_bot/Expenses/model/User.java
@@ -1,10 +1,21 @@
 package com.program.whatsapp_bot.Expenses.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
+@Table(name = "usuarios")
+@Data
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String waId;
+    private String nome;
 }
